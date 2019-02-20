bae8a39111e771e055305f299bace9dba2c3e7e2
K.Yoshida <kyoshida@sunbridge.com>
2019-02-20 18:55:29 +0900

    下記変更の移行
    作者: 高　文 <gaow@sunbridge.com>
    日時: 2018年10月19日 16:40:51
    コミット者: 高　文
    [update] SV_DEV-1279 IEでLEX画面「リードまたは取引先・取引先責任者に登録*」一括登録画面に遷移した際に「This page has an error.～～」とエラーメッセージが表示される対応

--- src/aura/SB_NCL_DataTableCell/SB_NCL_DataTableCellController.js
+++ src/aura/SB_NCL_DataTableCell/SB_NCL_DataTableCellController.js
@@ -14,7 +14,7 @@
  **/
 ({
   // 初期化
-	doInit : function(component, event, helper) {
+  doInit : function(component, event, helper) {
     var fieldName = component.get('v.field.fieldName');
     var cObjectMap = component.get('v.cObjectMap');
     component.set('v.value', cObjectMap[fieldName].value);
@@ -27,69 +27,71 @@
         var selectM = component.get('v.selectMap');
         if (recordId != null && selectM != null) {
           // alert(JSON.stringify(selectM[recordId].plickListOptions));
-          var plickOps = selectM[recordId][fieldName].plickListOptions;
+          var plickOps = selectM[fieldName].plickListOptions;
           if (value == null || value == '') {
-            if (fieldName == 'Lead') {
-              if (selectM[recordId]['Contact'] != null) {
-                var plickOps1 = selectM[recordId]['Contact'].plickListOptions;
-                if (plickOps1[0].value != 'new' && plickOps1[0].value != 'none') {
-                  cObjectMap[fieldName].value = 'none';
-                }
-                else {
-                  if (plickOps[0].value == 'new' && plickOps1[0].value == 'new') {
-                    var showType = component.get('v.showType');
-                    if (showType == 'Lead') {
-                      cObjectMap['Contact'].value = 'none';
-                      cObjectMap[fieldName].value = plickOps[0].value;
-                    }
-                    else {
-                      cObjectMap[fieldName].value = 'none';
-                    }
-                  }
-                  else {
-                    cObjectMap[fieldName].value = plickOps[0].value;
-                  }
-                  // cObjectMap[fieldName].value = plickOps[0].value;
-                }
-              }
-              else {
-                cObjectMap[fieldName].value = plickOps[0].value;
-              }
-            }
-            else if (fieldName == 'Contact') {
-              if (selectM[recordId]['Lead'] != null) {
-                var plickOps1 = selectM[recordId]['Lead'].plickListOptions;
-                if (plickOps1[0].value != 'new' && plickOps1[0].value != 'none'
-                  && (plickOps[0].value == 'new' || plickOps[0].value == 'none')) {
-                  cObjectMap[fieldName].value = 'none';
-                }
-                else {
-                  if (plickOps[0].value == 'new' && plickOps1[0].value == 'new') {
-                    var showType = component.get('v.showType');
-                    if (showType == 'Contact') {
-                      cObjectMap['Lead'].value = 'none';
-                      cObjectMap[fieldName].value = plickOps[0].value;
-                    }
-                    else {
-                      cObjectMap[fieldName].value = 'none';
-                    }
-                  }
-                  else {
-                    cObjectMap[fieldName].value = plickOps[0].value;
-                  }
-                  // cObjectMap[fieldName].value = plickOps[0].value;
-                }
-              }
-              else {
-                cObjectMap[fieldName].value = plickOps[0].value;
-              }
-            }
-            else {
-              cObjectMap[fieldName].value = plickOps[0].value;
-            }
-            component.set('v.cObjectMap', cObjectMap);
+            // component.set('v.value', cObjectMap[fieldName].value);
+
+            // if (fieldName == 'Lead') {
+            //   if (selectM['Contact'] != null) {
+            //     var plickOps1 = selectM['Contact'].plickListOptions;
+            //     if (plickOps1[0].value != 'new' && plickOps1[0].value != 'none') {
+            //       cObjectMap[fieldName].value = 'none';
+            //     }
+            //     else {
+            //       if (plickOps[0].value == 'new' && plickOps1[0].value == 'new') {
+            //         var showType = component.get('v.showType');
+            //         if (showType != 'Contact') {
+            //           cObjectMap['Contact'].value = 'none';
+            //           cObjectMap[fieldName].value = plickOps[0].value;
+            //         }
+            //         else {
+            //           cObjectMap[fieldName].value = 'none';
+            //         }
+            //       }
+            //       else {
+            //         cObjectMap[fieldName].value = plickOps[0].value;
+            //       }
+            //       // cObjectMap[fieldName].value = plickOps[0].value;
+            //     }
+            //   }
+            //   else {
+            //     cObjectMap[fieldName].value = plickOps[0].value;
+            //   }
+            // }
+            // else if (fieldName == 'Contact') {
+            //   if (selectM['Lead'] != null) {
+            //     var plickOps1 = selectM['Lead'].plickListOptions;
+            //     if (plickOps1[0].value != 'new' && plickOps1[0].value != 'none'
+            //       && (plickOps[0].value == 'new' || plickOps[0].value == 'none')) {
+            //       cObjectMap[fieldName].value = 'none';
+            //     }
+            //     else {
+            //       if (plickOps[0].value == 'new' && plickOps1[0].value == 'new') {
+            //         var showType = component.get('v.showType');
+            //         if (showType == 'Contact') {
+            //           cObjectMap['Lead'].value = 'none';
+            //           cObjectMap[fieldName].value = plickOps[0].value;
+            //         }
+            //         else {
+            //           cObjectMap[fieldName].value = 'none';
+            //         }
+            //       }
+            //       else {
+            //         cObjectMap[fieldName].value = plickOps[0].value;
+            //       }
+            //       // cObjectMap[fieldName].value = plickOps[0].value;
+            //     }
+            //   }
+            //   else {
+            //     cObjectMap[fieldName].value = plickOps[0].value;
+            //   }
+            // }
+            // else {
+            //   cObjectMap[fieldName].value = plickOps[0].value;
+            // }
+            // component.set('v.cObjectMap', cObjectMap);
           }
-          else if (selectM[recordId][fieldName].plickListValueMap != null && selectM[recordId][fieldName].plickListValueMap[value] == null) {
+          else if (selectM[fieldName].plickListValueMap != null && selectM[fieldName].plickListValueMap[value] == null) {
             cObjectMap[fieldName].value = plickOps[0].value;
           }
           // else if () {
@@ -104,9 +106,9 @@
       // }
     }
     var gid = component.getGlobalId();
-		component.set("v.gid", gid);
+    component.set("v.gid", gid);
 
-	},
+  },
   // 画面値変更イベント
   changeValue : function(component, event, helper) {
     // var ls = event.getSource().get('v.value');
