bc695bfe3f2936e38c0488af2af42e88b39834d4
K.Yoshida <kyoshida@sunbridge.com>
2019-02-21 14:53:23 +0900

    下記変更の移行
    作者: gaow <gaow@sunbridge.com>
    日時: 2018年11月27日 17:33:02
    コミット者: gaow
    [update] SV_DEV-1350 リードに登録（個別）、リードまたは・・・に登録（個別）でリードの参照権限のみの場合に「Attempt to de-reference a null object」エラーになる。

---
+++ src/aura/SB_NCL_BulkRegistComon/SB_NCL_BulkRegistComonHelper.js
@@ -0,0 +1,19 @@
+/**
+ *
+ *  SB_NCL_BulkRegistComon
+ *  リード拡張環境 Lightning統合用共通コンポネート Helper
+ *
+ *
+ *
+ *
+ *  Copyright (C) 2018 SunBridge Inc. All Rights Reserved.
+ *
+ *  @author mao
+ *  @Version 1.12      2017.05.XX SV_DEV-511 [LEX]リードの名刺で更新のLightning版対応
+ *
+ **/
+({
+	helperMethod : function() {
+		
+	}
+})
---
+++ src/aura/SB_NCL_LeadBulkRegist/SB_NCL_LeadBulkRegistController.js
@@ -0,0 +1,61 @@
+/**
+ *
+ *  SB_NCL_LeadBulkRegist
+ *  リード拡張環境 一括統合用コンポネート Controller
+ *
+ *
+ *
+ *
+ *  Copyright (C) 2018 SunBridge Inc. All Rights Reserved.
+ *
+ *  @author mao
+ *  @Version 拡張パッケージ：Lead Ex. 1.12      2017.05.XX 初版
+ *
+ **/
+({
+  doInit : function (cmp, event, helper) {
+      var targetIds = cmp.get("v.recordId");
+      cmp.set('v.working', true);
+      cmp.set('v.showErrorMsg', false);
+      if (targetIds != null) {
+        helper.searchLead(cmp, event, helper, targetIds);
+        helper.setFieldList(cmp, event, helper);
+        helper.setTitleValue(cmp, event, helper);
+        helper.setInputValue(cmp, event, helper);
+        helper.doInit(cmp, event, helper, targetIds);
+      }
+      else {
+        cmp.set('v.working', false);
+      }
+    },
+    changeSearchCondition : function (cmp, event, helper) {
+      // helper.changeSearchCondition(cmp, event, helper);
+      cmp.set('v.working', true);
+      var targetIds = cmp.get('v.recordId');
+      helper.searchLead(cmp, event, helper, targetIds);
+    },
+    doSomething : function (cmp, event, helper) {
+      var name = event.getParam('name');
+      if (name == 'close') {
+        sforce.one.navigateToURL(cmp.get('v.retUrl'));
+        cmp.set('v.working', false);
+      }
+      else if (name == 'save') {
+        cmp.set('v.working', true);
+        helper.save(cmp, event, helper);
+      }
+      else if (name == 'changeCampaign') {
+        helper.changeCampaign(cmp, event, helper);
+      }
+      else if (name == 'cleanCampaign') {
+        var inpv = cmp.get("v.inputV");
+        inpv.campaignStatusPlick.plickListOptions = null;
+        inpv.campaignStatusPlick.selectValue = null;
+        cmp.set("v.inputV", inpv);
+      } else if (name == 'cleanOwner') {
+        var inv = cmp.get("v.inputV");
+        inv.ownerId = null;
+        cmp.set("v.inputV", inv);
+      }
+    }
+})
